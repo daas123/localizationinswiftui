ba2a8e870af52322b8e99070da9dc05ca6ba88e9
saad <saadneosoft@gmail.com>
2023-11-27 17:57:30 +0530

    Initial Commit

---
+++ localizationinswiftui/localizationinswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  localizationinswiftuiApp.swift
+//  localizationinswiftui
+//
+//  Created by Neosoft on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct localizationinswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
